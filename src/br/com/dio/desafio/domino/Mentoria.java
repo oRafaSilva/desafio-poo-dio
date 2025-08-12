105f543f8baa80476b54a5722e0437d2194246cc
rafaSilva <o.rafaels023@gmail.com>
2025-08-12 10:02:28 -0300

    Criação das classes Curso, Mentoria e Conteudo

--- src/br/com/dio/desafio/domino/Mentoria.java
+++ src/br/com/dio/desafio/domino/Mentoria.java
@@ -2,27 +2,19 @@ package br.com.dio.desafio.domino;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
+    
     private LocalDate data;
 
-    public Mentoria(){
-        
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public Mentoria(){
+
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public LocalDate getData() {
         return data;
     }
@@ -32,8 +24,10 @@ public class Mentoria {
 
     @Override
     public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
     }
+
+    
 
     
     
